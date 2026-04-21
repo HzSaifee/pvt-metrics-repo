479fb4d71e449e1706ad35fb481d78068f32c220
huzefa.saifee <huzefa.saifee@workday.com>
2026-04-21 08:27:41 -0600

    Updated Workday GO queries to be a bit more inclusive

--- airflow/dags/team_hive/workday_go_accounts/snowflake_workday_go.sql
+++ airflow/dags/team_hive/workday_go_accounts/snowflake_workday_go.sql
@@ -1,14 +1,4 @@
 SELECT DISTINCT
   "OPPORTUNITY"."ACCOUNTID"
 FROM "BASE_PROD"."SALESFORCE"."OPPORTUNITY" "OPPORTUNITY"
-WHERE CAST("OPPORTUNITY"."EXTERNAL_SELECTED_PACKAGE_S__C" AS TEXT) IN (
-    'Workday GO Deployment',
-    'Workday GO Frontline Worker',
-    'Workday GO Global Payroll',
-    'Workday GO for HR',
-    'Workday Go for Finance - LDP',
-    'Workday Go for HR - LDP',
-    'Workday Go for HR - LDP;Workday GO for HR',
-    'Workday Go for HR - LDP;Workday GO for HR;Workday Go Global Payroll;Workday Go Deployment',
-    'Workday Go for HR - LDP;Workday Go for Finance - LDP'
-)
+WHERE LOWER(CAST("OPPORTUNITY"."EXTERNAL_SELECTED_PACKAGE_S__C" AS TEXT)) LIKE '%workday go%'
